dfa7f429156bdf2cb7e35b771c056459dbe6ebee
Brian Tang <ashrielbrian@gmail.com>
2023-12-28 00:18:01 +0800

    Tests module import works

--- src/indexes/mod.rs
+++ src/indexes/mod.rs
@@ -1,2 +1,3 @@
 pub mod base;
 pub mod ivfflat;
+pub mod lsh;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use vers::indexes::ivfflat::IVFFlatIndex;
+use vers::indexes::lsh::{ANNIndex, Vector};
 fn main() {
     println!("Hello, world!");
+
+    let val = Vector([1.0, 2.0, 3.0]);
+    println!("{:?}", val);
 }
