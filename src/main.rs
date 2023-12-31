f6480d44d86193338fd8aabbf698b55679c56d89
Brian Tang <ashrielbrian@gmail.com>
2023-12-31 23:22:36 +0800

    Bunch of tests on both indexes with adding new embeds

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
 use itertools::Itertools;
 use rand::seq::SliceRandom;
-use rand::Rng;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs;
 use std::io::BufRead;
+use vers::indexes::base::{Index, Vector};
 use vers::indexes::ivfflat::IVFFlatIndex;
-use vers::indexes::lsh::{ANNIndex, Vector};
+use vers::indexes::lsh::ANNIndex;
 
 fn search_exhaustive<const N: usize>(
     vector_data: &Vec<Vector<N>>,
     query: &Vector<N>,
     top_k: usize,
 ) -> Vec<(usize, f32)> {
-    let candidates: Vec<(usize, f32)> = vector_data
+    vector_data
         .iter()
         .enumerate()
         .map(|(i, v)| (i, v.squared_euclidean(&query)))
         .sorted_by(|a, b| a.1.partial_cmp(&b.1).unwrap())
         .take(top_k)
-        .collect();
-
-    candidates
+        .collect()
 }
 
 fn load_wiki_vector<const N: usize>(
@@ -29,6 +27,7 @@ fn load_wiki_vector<const N: usize>(
     Vec<Vector<N>>,
     HashMap<String, usize>,
     HashMap<usize, String>,
+    Vec<(String, [f32; N])>,
 ) {
     let vector_file = fs::File::open(file_path).expect("Should be able to read file");
     let reader = std::io::BufReader::new(vector_file);
@@ -38,16 +37,13 @@ fn load_wiki_vector<const N: usize>(
     let mut word_to_idx: HashMap<String, usize> = HashMap::new();
     let mut idx_to_word: HashMap<usize, String> = HashMap::new();
 
+    let mut embs: Vec<(String, [f32; N])> = Vec::new();
+
     for wrapped_line in reader.lines().skip(1) {
         let line = wrapped_line.unwrap();
 
         let mut split_by_spaces = line.split_whitespace();
         let word = split_by_spaces.next().unwrap();
-        word_to_idx.insert(word.to_owned(), curr_idx);
-        idx_to_word.insert(curr_idx, word.to_owned());
-
-        curr_idx += 1;
-
         let emb: [f32; N] = split_by_spaces
             .into_iter()
             .map(|d| d.parse::<f32>().unwrap())
@@ -55,7 +51,30 @@ fn load_wiki_vector<const N: usize>(
             .try_into()
             .unwrap();
 
+        if word == "queen"
+            || word == "princess"
+            || word == "kings"
+            || word == "princes"
+            || word == "King"
+            || word == "monarch"
+            || word == "Prince"
+        {
+            println!("{} {}", word, curr_idx);
+            embs.push((word.to_string(), emb));
+            continue;
+        }
+
+        word_to_idx.insert(word.to_owned(), curr_idx);
+        idx_to_word.insert(curr_idx, word.to_owned());
+
+        curr_idx += 1;
         all_vecs.push(Vector(emb))
+    }
+
+    // verify does not contain `queen`
+    match word_to_idx.get("queen") {
+        Some(_) => println!("Found queen!"),
+        None => println!("No queen, not to worry!"),
     }
 
     println!(
@@ -64,7 +83,7 @@ fn load_wiki_vector<const N: usize>(
         word_to_idx.len(),
         idx_to_word.len()
     );
-    (all_vecs, word_to_idx, idx_to_word)
+    (all_vecs, word_to_idx, idx_to_word, embs)
 }
 
 fn build_index<const N: usize>(
@@ -73,10 +92,11 @@ fn build_index<const N: usize>(
     max_node_size: usize,
     word_to_idx: &HashMap<String, usize>,
     idx_to_word: &HashMap<usize, String>,
-) -> ANNIndex<N> {
+) -> IVFFlatIndex<N> {
     let vector_ids: Vec<usize> = (0..vector_data.len()).collect();
 
-    let index = ANNIndex::build_index(num_trees, max_node_size, vector_data, &vector_ids);
+    // let index = ANNIndex::build_index(num_trees, max_node_size, vector_data, &vector_ids);
+    let index: IVFFlatIndex<N> = IVFFlatIndex::build_index(5, 4, 5, vector_data);
 
     let benchmark_idxs: Vec<&usize> = vector_ids
         .choose_multiple(&mut rand::thread_rng(), 10)
@@ -135,10 +155,57 @@ fn build_index<const N: usize>(
 }
 fn main() {
     const DIM: usize = 300;
-    let (wiki, word_to_idx, idx_to_word) = load_wiki_vector::<DIM>("wiki-news-300d-1M.vec");
+    let (wiki, mut word_to_idx, mut idx_to_word, embs) =
+        load_wiki_vector::<DIM>("wiki-news-300d-1M.vec");
 
-    let index = build_index(&wiki, 8, 250, &word_to_idx, &idx_to_word);
+    let mut index = build_index(&wiki, 8, 6, &word_to_idx, &idx_to_word);
 
-    let val = Vector([1.0, 2.0, 3.0]);
-    println!("{:?}", val);
+    // NOT THIS!!
+    for ((word, emb), vec_id) in embs
+        .into_iter()
+        .zip([999987, 999988, 999989, 999990, 999991, 999992, 999993])
+    {
+        println!("Inserting {} {}", word, vec_id);
+        idx_to_word.insert(vec_id, word.to_string());
+        index.add(Vector(emb), vec_id);
+    }
+
+    println!("QUEENNNNNN ----------------------------------");
+    let selected_words = ["king", "prince"];
+    let selected_vector_ids: Vec<&usize> = selected_words
+        .iter()
+        .map(|w| word_to_idx.get(*w).unwrap())
+        .collect();
+
+    for (vec_id, search_word) in selected_vector_ids.iter().zip(selected_words) {
+        let results = index.search_approximate(wiki[**vec_id], 5);
+
+        println!("---> SEARCH APPRX WORD: {search_word}");
+        for (i, (results_idx, distance)) in results.iter().enumerate() {
+            println!(
+                "{i}. Word: {}. Distance: {}",
+                idx_to_word.get(results_idx).unwrap(),
+                distance.sqrt()
+            )
+        }
+    }
+
+    match index.save_index("here.index") {
+        Ok(_) => println!("Index saved successfully!"),
+        Err(e) => eprintln!("Index save failed: {}", e),
+    }
+
+    let reload_index: IVFFlatIndex<DIM> = match IVFFlatIndex::load_index("here.index") {
+        Ok(index) => index,
+        Err(e) => panic!("Failed to load index! {}", e),
+    };
+
+    let results = reload_index.search_approximate(wiki[*word_to_idx.get("king").unwrap()], 20);
+    for (i, (results_idx, distance)) in results.iter().enumerate() {
+        println!(
+            "{i}. Word: {}. Distance: {}",
+            idx_to_word.get(results_idx).unwrap(),
+            distance.sqrt()
+        )
+    }
 }
