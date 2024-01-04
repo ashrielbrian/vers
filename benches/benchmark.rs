eff3f5954028c73f39319644b2841d8f629ad6bd
Brian Tang <ashrielbrian@gmail.com>
2024-01-05 01:10:13 +0800

    Reduces sample size from default 100 to 20

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -6,12 +6,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     const DIM: usize = 300;
     let (wiki, _, _, _) = utils::load_wiki_vector::<DIM>("wiki-news-300d-1M.vec");
 
-    for num_cluster in [5, 10, 25] {
+    for num_cluster in [1, 10] {
         c.bench_function(format!("IVFFlat: {} clusters", num_cluster).as_str(), |b| {
-            b.iter(|| IVFFlatIndex::build_index(num_cluster, 3, 10, &wiki))
+            b.iter(|| IVFFlatIndex::build_index(num_cluster, 2, 5, &wiki))
         });
     }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name=benches;
+    config=Criterion::default().sample_size(20);
+    targets=criterion_benchmark
+}
 criterion_main!(benches);
