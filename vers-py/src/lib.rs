6ee5dee267ae05dc7b67a9923a7fbe1438c614f9
Brian Tang <ashrielbrian@gmail.com>
2024-01-19 00:51:29 +0800

    Adds python binding to run test ivfflat func

--- vers-py/src/lib.rs
+++ vers-py/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cell::RefCell, collections::HashMap};
 
 use pyo3::prelude::*;
 
@@ -34,6 +34,34 @@ fn load_wiki_vector(
 }
 
 #[pyfunction]
+fn test_wiki_ivfflat(
+    vectors: Vec<Vector_F32_N300>,
+    word_to_idx: HashMap<String, usize>,
+    idx_to_word: HashMap<usize, String>,
+    num_clusters: usize,
+    num_attempts: usize,
+    max_iterations: usize,
+    test_embs: Vec<(String, [f32; 300])>,
+) {
+    let vectors: Vec<Vector<300>> = vectors.iter().map(|v| v.0).collect();
+
+    Python::with_gil(|py| {
+        let obj = RefCell::new(idx_to_word);
+        let mut idx_to_word_borrowed = obj.borrow_mut();
+
+        utils::test_ivfflat(
+            &vectors,
+            &word_to_idx,
+            &mut idx_to_word_borrowed,
+            num_clusters,
+            num_attempts,
+            max_iterations,
+            &test_embs,
+        );
+    })
+}
+
+#[pyfunction]
 fn get_sum(v1: [f32; 300], v2: [f32; 300]) -> PyResult<Vector_F32_N300> {
     let v1 = Vector_F32_N300(Vector(v1));
     let v2 = Vector_F32_N300(Vector(v2));
@@ -45,5 +73,6 @@ fn get_sum(v1: [f32; 300], v2: [f32; 300]) -> PyResult<Vector_F32_N300> {
 fn vers_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(get_sum, m)?)?;
     m.add_function(wrap_pyfunction!(load_wiki_vector, m)?)?;
+    m.add_function(wrap_pyfunction!(test_wiki_ivfflat, m)?)?;
     Ok(())
 }
