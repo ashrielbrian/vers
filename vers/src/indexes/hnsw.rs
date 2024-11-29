4f0476546420d8fa76062f4afbcd5785e8ced6e2
Brian Tang <ashrielbrian@gmail.com>
2024-11-29 18:42:46 +0800

    Adds private search layer function

--- vers/src/indexes/hnsw.rs
+++ vers/src/indexes/hnsw.rs
@@ -2,7 +2,8 @@ use serde::{Deserialize, Serialize};
 
 use super::base::Index;
 use crate::Vector;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 
 #[derive(Serialize, Deserialize)]
 pub struct HNSWIndex<const N: usize> {
@@ -15,6 +16,26 @@ pub struct HNSWIndex<const N: usize> {
 pub struct HNSWLayer<const N: usize> {
     adjacency_list: HashMap<String, Vec<String>>,
     nodes: HashMap<String, Vec<Vector<N>>>,
+}
+
+#[derive(PartialEq)]
+struct DistanceMaxCandidatePair {
+    candidate_id: String,
+    distance: f32,
+}
+
+impl Eq for DistanceMaxCandidatePair {}
+
+impl PartialOrd for DistanceMaxCandidatePair {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.distance.partial_cmp(&other.distance)
+    }
+}
+
+impl Ord for DistanceMaxCandidatePair {
+    fn cmp(&self, other: &DistanceMaxCandidatePair) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
 }
 
 impl<const N: usize> HNSWIndex<N> {
@@ -39,14 +60,43 @@ impl<const N: usize> HNSWIndex<N> {
         todo!()
     }
 
-    pub fn _search_layer(entrypoint: Node<N>, query_vector: Vector<N>, layer: &HNSWLayer<N>) {
+    pub fn _search_layer(
+        entrypoint: Node<N>,
+        query_vector: Vector<N>,
+        layer: &HNSWLayer<N>,
+        ef_construction: usize,
+        id_to_vec: &HashMap<String, Vector<N>>,
+    ) -> Vec<(String, f32)> {
         // returns a list of candidates closest to the query vector for a given layer
         let mut candidates: Vec<(String, f32)> = vec![];
         let mut queue: VecDeque<String> = VecDeque::new();
+        let mut candidates_heap: BinaryHeap<DistanceMaxCandidatePair> = BinaryHeap::new();
 
         queue.push_back(entrypoint.id);
 
-        while let Some(node) = queue.pop_front() {}
+        while let Some(node) = queue.pop_front() {
+            if let Some(neighbour_ids) = layer.adjacency_list.get(&node) {
+                for neighbour_id in neighbour_ids {
+                    let neighbour_dist =
+                        query_vector.squared_euclidean(&id_to_vec.get(neighbour_id).unwrap());
+
+                    // if the current neighbour distance is smaller than the candidate with the largest distance, replace
+                    // this candidate with the current neighbour
+                    if candidates.len() < ef_construction
+                        || (candidates_heap.len() > 0
+                            && neighbour_dist < candidates_heap.peek().unwrap().distance)
+                    {
+                        queue.push_back(neighbour_id.clone());
+                        candidates_heap.push(DistanceMaxCandidatePair {
+                            candidate_id: neighbour_id.clone(),
+                            distance: neighbour_dist,
+                        });
+                    }
+                }
+            }
+        }
+
+        candidates
     }
 }
 
