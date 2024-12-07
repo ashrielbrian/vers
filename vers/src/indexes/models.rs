474671f5c56b816b0c18105f223d4033678d85b5
Brian Tang <ashrielbrian@gmail.com>
2024-12-07 20:06:36 +0800

    Adds AdjacencyItem abstraction

---
+++ vers/src/indexes/models.rs
@@ -0,0 +1,128 @@
+use serde::{Deserialize, Serialize};
+use std::cmp::{min, Ordering};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
+use std::hash::{Hash, Hasher};
+
+#[derive(PartialEq)]
+pub struct DistanceMaxCandidatePair<'a> {
+    pub candidate_id: &'a usize,
+    pub distance: f32,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct DistanceCandidatePair {
+    pub candidate_id: usize,
+    pub distance: f32,
+}
+
+impl Eq for DistanceMaxCandidatePair<'_> {}
+
+impl<'a> PartialOrd for DistanceMaxCandidatePair<'a> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.distance.partial_cmp(&other.distance)
+    }
+}
+
+impl<'a> Ord for DistanceMaxCandidatePair<'a> {
+    fn cmp(&self, other: &DistanceMaxCandidatePair) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+// we need the following traits so we can use DistanceCandidatePair within a HashSet
+impl Hash for DistanceCandidatePair {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.candidate_id.hash(state);
+    }
+}
+
+impl Eq for DistanceCandidatePair {}
+
+impl PartialEq for DistanceCandidatePair {
+    fn eq(&self, other: &Self) -> bool {
+        self.candidate_id == other.candidate_id
+    }
+}
+
+impl<'a> PartialOrd for DistanceCandidatePair {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.distance.partial_cmp(&other.distance)
+    }
+}
+
+impl Ord for DistanceCandidatePair {
+    fn cmp(&self, other: &DistanceCandidatePair) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+pub struct AdjacencyItem {
+    pub max_heap: BinaryHeap<DistanceCandidatePair>,
+    pub neighbours: HashSet<usize>,
+}
+
+impl AdjacencyItem {
+    pub fn new() -> Self {
+        AdjacencyItem {
+            max_heap: BinaryHeap::new(),
+            neighbours: HashSet::new(),
+        }
+    }
+    pub fn insert(&mut self, u: &DistanceCandidatePair) {
+        self.neighbours.insert(u.candidate_id.clone());
+        self.max_heap.push(u.clone());
+    }
+
+    pub fn len(&self) -> usize {
+        self.neighbours.len()
+    }
+
+    pub fn trim(&mut self, max_neighbours: usize) {
+        // removes elements with the largest distances first until <= num_neighbours
+        while self.max_heap.len() > max_neighbours {
+            let to_remove = self.max_heap.pop().unwrap();
+            self.neighbours.remove(&to_remove.candidate_id);
+        }
+    }
+}
+
+impl Serialize for AdjacencyItem {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        // Since BinaryHeap does not implement Serialize/Deserialize, we need to convert it to a Vec first.
+        let max_heap_vec: Vec<DistanceCandidatePair> = self.max_heap.clone().into_sorted_vec();
+        let neighbours_vec: Vec<usize> = self.neighbours.iter().cloned().collect();
+
+        let adj_item = AdjacencyItemSer {
+            max_heap: max_heap_vec,
+            neighbours: neighbours_vec,
+        };
+
+        adj_item.serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for AdjacencyItem {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let adj_item = AdjacencyItemSer::deserialize(deserializer)?;
+
+        let max_heap = BinaryHeap::from(adj_item.max_heap);
+        let neighbours = adj_item.neighbours.into_iter().collect();
+
+        Ok(AdjacencyItem {
+            max_heap,
+            neighbours,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct AdjacencyItemSer {
+    max_heap: Vec<DistanceCandidatePair>,
+    neighbours: Vec<usize>,
+}
