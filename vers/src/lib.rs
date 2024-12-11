c466aaa7329c250783b7c8da0a4e1481a996c7ee
Brian Tang <ashrielbrian@gmail.com>
2024-12-12 00:55:04 +0800

    Rough testing of hnsw

--- vers/src/lib.rs
+++ vers/src/lib.rs
@@ -5,3 +5,4 @@ pub use indexes::base::{Index, Vector};
 pub use indexes::hnsw::HNSWIndex;
 pub use indexes::ivfflat::IVFFlatIndex;
 pub use indexes::lsh::ANNIndex;
+pub use indexes::models::{AdjacencyItem, DistanceCandidatePair};
